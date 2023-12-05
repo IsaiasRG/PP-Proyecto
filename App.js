3aaa42e7c7d9a334f3684af592aff72eaa0318c0
Isaias Ramirez :) <isaias.ramirez2004@gmail.com>
2023-12-05 20:42:19 -0300

    agregando inputs

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Proyecto Planeta Paleontologico - By Isaias Ramirez</Text>
+      <View style={styles.InputContainer}>
+       <TextInput style={styles.Input}/>
+       <Button title="+"/>
+      </View>
+      <Text style={styles.Text}>Proyecto Planeta Paleontologico - By Isaias Ramirez</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop:30,
+    backgroundColor: '#386150',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  InputContainer:{
+    flexDirection:"row",
+    width:"100%",
+    justifyContent:"space-around",
+    backgroundColor:"#3D3522"
+  },
+  Input: {
+    borderWidth:1,
+    borderColor:"#41533F",
+
+  },
+
+  Text:{
+    color:"#B1DAD1"
+  }
 });
